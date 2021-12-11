d12fff288ca37267d309f7c12e7632750b72aae2
Aleksandras Liauska <aleksandras.v.liauska@bath.edu>
2021-12-11 15:33:04 +0200

    push to git

--- Functions.py
+++ Functions.py
@@ -47,3 +47,33 @@ def start_of_month(my_date):
     starting_date = starting_date.strftime('%Y-%m-%d')
     return starting_date
 
+
+def start_date_six(my_date):
+
+    """
+    Gives the starting date of a month given any date
+    :param my_date: date, str
+    :return: str
+    """
+
+    my_date = datetime.strptime(my_date, '%Y-%m-%d')
+    starting_date = date(my_date.year, 6, 1)
+    starting_date = starting_date.strftime('%Y-%m-%d')
+    return starting_date
+
+def semi_annual_cov(period, my_date, my_data, tickers):
+
+    """
+    Calculates a covariance matrix given the following parameters:
+    :param period: training period in years, int
+    :param my_date: first day of the trading year, str
+    :param my_data: dataframe of prices and dates
+    :param tickers: list of tickers
+    :return: returns a covariance matrix dataframe
+    """
+    np.random.seed(42)
+    covariance_matrix = pd.DataFrame(np.random.randn(126*period, len(tickers)),
+                                     index=pd.date_range(my_date, periods=126*period),
+                                     columns=list(my_data)).rolling(126*period).cov().dropna().droplevel(0, axis=0)
+    return covariance_matrix
+
